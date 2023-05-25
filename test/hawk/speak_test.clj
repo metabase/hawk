ecc0a10eecd3785e59b3612ebaaabc276baf7f55
John Cromartie <john@metabase.com>
2023-05-25 14:23:10 -0400

    Removes println debugging

--- test/hawk/speak_test.clj
+++ test/hawk/speak_test.clj
@@ -8,7 +8,6 @@
                             (with-redefs [hawk.speak/enabled? (constantly true)
                                           sh/sh (fn [& args] (reset! sh-args (vec args)))]
                               (hawk.speak/handle-event! (assoc summary :type :summary))
-                              (println "sh-args =" @sh-args)
                               (= expected @sh-args)))
     {:error 0 :fail 0} ["say" "all tests passed"]
     {:error 1 :fail 0} ["say" "tests failed" "1 error"]
